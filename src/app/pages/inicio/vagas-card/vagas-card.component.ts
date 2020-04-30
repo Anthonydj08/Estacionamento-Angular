f61b74e93642bcc05b44d9a4b79e2f1fafb121fe
Anthony Douglas <31930381+Anthonydj08@users.noreply.github.com>
2020-04-30 11:55:33 -0300

    Gráfico da tela inicial funcionando

--- src/app/pages/inicio/vagas-card/vagas-card.component.ts
+++ src/app/pages/inicio/vagas-card/vagas-card.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input, AfterViewInit, OnDestroy } from '@angular/cor
 import { takeWhile, delay } from 'rxjs/operators';
 import { LayoutService } from '../../../@core/utils';
 import { NbThemeService } from '@nebular/theme';
+import { DbService } from '../../../@core/services/db.service';
+import { numVagas } from '../../../@core/model/numVagas';
+import { EntradaESaida } from '../../../@core/model/entradaESaida';
 
 @Component({
   selector: 'ngx-vagas-card',
@@ -11,70 +14,111 @@ import { NbThemeService } from '@nebular/theme';
 export class VagasCardComponent implements AfterViewInit, OnDestroy {
   options: any = {};
   themeSubscription: any;
+  numVagas: numVagas[];
+  entradaESaidas: EntradaESaida[];
 
-  constructor(private theme: NbThemeService) {
+  disponiveis: any;
+  ocupadas: any;
+
+  constructor(private theme: NbThemeService, private dbService: DbService, ) {
   }
 
-  ngAfterViewInit() {
-    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+  private async load() {
+    await this.dbService.listWithUIDs<numVagas>('/numVagas')
+      .then(numVagas => {
+        this.numVagas = numVagas;
+        console.log(numVagas);
+      }).catch(error => {
+        console.log(error);
+      });
 
-      const colors = config.variables;
-      const echarts: any = config.variables.echarts;
+    await this.dbService.listWithUIDs<EntradaESaida>('/entradaesaida')
+      .then(entradaESaidas => {
+        this.entradaESaidas = entradaESaidas;
+      }).catch(error => {
+        console.log(error);
+      });
+  }
 
-      this.options = {
-        backgroundColor: echarts.bg,
-        color: [colors.successLight, colors.dangerLight],
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)',
-        },
-        legend: {
-          orient: 'vertical',
-          left: 'left',
-          data: ['Disponíveis', 'Ocupadas'],
-          textStyle: {
-            color: echarts.textColor,
-          },
-        },
-        series: [
-          {
-            name: 'Vagas',
-            type: 'pie',
-            radius: ['50%', '70%'],
-            center: ['50%', '50%'],
-            data: [
-              { value: 114, name: 'Disponíveis' },
-              { value: 36, name: 'Ocupadas' },
+  private async atualizar() {
+    await this.load()
+      .then(num => {
+        this.disponiveis = this.numVagas[0].vagas
+        this.ocupadas = 0
+        console.log(this.entradaESaidas);
+        for (let i = 0; i < this.entradaESaidas.length; i++) {
+          if (!this.entradaESaidas[i].saida) {
+            this.ocupadas++
+          }
+        }
+      })
+  }
+
+  async ngAfterViewInit() {
+    await this.atualizar()
+      .then(num => {
+
+
+        this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+          const colors = config.variables;
+          const echarts: any = config.variables.echarts;
+
+          this.options = {
+            backgroundColor: echarts.bg,
+            color: [colors.successLight, colors.dangerLight],
+            tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b} : {c} ({d}%)',
+            },
+            legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: ['Disponíveis', 'Ocupadas'],
+              textStyle: {
+                color: echarts.textColor,
+              },
+            },
+            series: [
+              {
+                name: 'Vagas',
+                type: 'pie',
+                radius: ['50%', '70%'],
+                center: ['50%', '50%'],
+                data: [
+                  { value: this.disponiveis, name: 'Disponíveis' },
+                  { value: this.ocupadas, name: 'Ocupadas' },
+                ],
+                itemStyle: {
+                  emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: echarts.itemHoverShadowColor,
+                  },
+                },
+                label: {
+                  normal: {
+                    show: false,
+                    position: 'center'
+                  },
+                  emphasis: {
+                    show: true,
+                    textStyle: {
+                      fontSize: '25',
+                      fontWeight: 'bold'
+                    }
+                  }
+                },
+                labelLine: {
+                  normal: {
+                    show: false
+                  }
+                },
+              },
             ],
-            itemStyle: {
-              emphasis: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: echarts.itemHoverShadowColor,
-              },
-            },
-            label: {
-              normal: {
-                show: false,
-                position: 'center'
-              },
-              emphasis: {
-                show: true,
-                textStyle: {
-                  fontSize: '25',
-                  fontWeight: 'bold'
-                }
-              }
-            },
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-          },
-        ],
-      };
-    });
+          };
+        });
+      })
   }
 
   ngOnDestroy(): void {
